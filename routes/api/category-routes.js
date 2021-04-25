89cb64f874911d99ba414365a19e99eb5092da95
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-25 11:10:40 -0400

    Add put requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,6 +46,22 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value NEEDS REVIEW
+  try {
+    const categoryData = await Category.update({
+      title: req.body.title,
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
