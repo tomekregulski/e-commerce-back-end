e09f48e029ea2dde4c80726f1c16d0eceb4ef7e0
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-25 10:17:37 -0400

    Build models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) CHECK THIS
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, unqiue: false },
+  as: "product_tag",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) CHECK THIS
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag, unqiue: false },
+  as: "product_tag",
+});
 
 module.exports = {
   Product,
